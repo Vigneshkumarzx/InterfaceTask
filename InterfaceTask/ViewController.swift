d07afae6d6aad953b56772758ab4735d8c98b8d6
vigneshkumarzx <vigneshzx@gmail.com>
2022-02-03 17:54:11 +0530

    third commit

--- InterfaceTask/ViewController.swift
+++ InterfaceTask/ViewController.swift
@@ -17,13 +17,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var ttlAmntLbl: UILabel!
     @IBOutlet weak var expensiveCollectionView: UICollectionView!
     
-    var expensiveDetails:[ExpensiveDetails] = [.init(itemImage: "https://i.pinimg.com/originals/3b/fb/e3/3bfbe3cfaab065615942f4b77595b762.jpg", itemName: "Uber Ride", date: "FEB 02,2022", itemPrice: "1650", status: "Approved"),
-        .init(itemImage: "https://miro.medium.com/max/1200/1*AdiawHSAn_LXYOxSSxfLNA.png", itemName: "Starbucks", date: "FEB 02,2022", itemPrice: "3434", status: "pending"),
-        .init(itemImage: "https://play-lh.googleusercontent.com/7g1mR_Od0qNRfiEN4MsSSKyb8d1MB3GrvShoKKOCrGituQ2d73IMcnim9J3lHlWDHJA", itemName: "Tickets New ", date: "FEB 02,2022", itemPrice: "1500", status: "pending"),
-        .init(itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/1200px-Domino%27s_pizza_logo.svg.png", itemName: "Dominos Pizza", date: "FEB 02,2022", itemPrice: "1250", status: "Approved"),
-        .init(itemImage: "https://logopond.com/logos/e540d0738a465734593c9684ce898210.png", itemName: "BurgerHut", date: "FEB 02,2022", itemPrice: "2750", status: "Approved")
+    
+    
+    var expensiveDetails:[ExpensiveDetails] = []
+    
+    var weeklyExpensiveDetails:[ExpensiveDetails] = [
+      
+        .init(itemImage: "https://i.pinimg.com/originals/3b/fb/e3/3bfbe3cfaab065615942f4b77595b762.jpg", itemName: "Uber Ride", date: "FEB 02,2022", itemPrice: "650", status: "Approved"),
+            .init(itemImage: "https://miro.medium.com/max/1200/1*AdiawHSAn_LXYOxSSxfLNA.png", itemName: "Starbucks", date: "FEB 02,2022", itemPrice: "434", status: "pending"),
+            .init(itemImage: "https://play-lh.googleusercontent.com/7g1mR_Od0qNRfiEN4MsSSKyb8d1MB3GrvShoKKOCrGituQ2d73IMcnim9J3lHlWDHJA", itemName: "Tickets New ", date: "FEB 02,2022", itemPrice: "500", status: "pending"),
+            .init(itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/1200px-Domino%27s_pizza_logo.svg.png", itemName: "Dominos Pizza", date: "FEB 02,2022", itemPrice: "1050", status: "Approved"),
+            .init(itemImage: "https://logopond.com/logos/e540d0738a465734593c9684ce898210.png", itemName: "BurgerHut", date: "FEB 02,2022", itemPrice: "750", status: "Approved")
+    ]
+    var DailyExpensiveDetails:[ExpensiveDetails] = [
+      
+        .init(itemImage: "https://i.pinimg.com/originals/3b/fb/e3/3bfbe3cfaab065615942f4b77595b762.jpg", itemName: "Uber Ride", date: "FEB 02,2022", itemPrice: "100", status: "Approved"),
+            .init(itemImage: "https://miro.medium.com/max/1200/1*AdiawHSAn_LXYOxSSxfLNA.png", itemName: "Starbucks", date: "FEB 02,2022", itemPrice: "250", status: "pending"),
+            .init(itemImage: "https://play-lh.googleusercontent.com/7g1mR_Od0qNRfiEN4MsSSKyb8d1MB3GrvShoKKOCrGituQ2d73IMcnim9J3lHlWDHJA", itemName: "Tickets New ", date: "FEB 02,2022", itemPrice: "180", status: "pending"),
+            .init(itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/1200px-Domino%27s_pizza_logo.svg.png", itemName: "Dominos Pizza", date: "FEB 02,2022", itemPrice: "40", status: "Approved"),
+            .init(itemImage: "https://logopond.com/logos/e540d0738a465734593c9684ce898210.png", itemName: "BurgerHut", date: "FEB 02,2022", itemPrice: "300", status: "Approved")
+    
     ]
     
+ 
+    var MonthlyExpensiveDetails:[ExpensiveDetails] = [
+        .init(itemImage: "https://i.pinimg.com/originals/3b/fb/e3/3bfbe3cfaab065615942f4b77595b762.jpg", itemName: "Uber Ride", date: "FEB 02,2022", itemPrice: "1650", status: "Approved"),
+            .init(itemImage: "https://miro.medium.com/max/1200/1*AdiawHSAn_LXYOxSSxfLNA.png", itemName: "Starbucks", date: "FEB 02,2022", itemPrice: "3434", status: "pending"),
+            .init(itemImage: "https://play-lh.googleusercontent.com/7g1mR_Od0qNRfiEN4MsSSKyb8d1MB3GrvShoKKOCrGituQ2d73IMcnim9J3lHlWDHJA", itemName: "Tickets New ", date: "FEB 02,2022", itemPrice: "1500", status: "pending"),
+            .init(itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/1200px-Domino%27s_pizza_logo.svg.png", itemName: "Dominos Pizza", date: "FEB 02,2022", itemPrice: "1250", status: "Approved"),
+            .init(itemImage: "https://logopond.com/logos/e540d0738a465734593c9684ce898210.png", itemName: "BurgerHut", date: "FEB 02,2022", itemPrice: "2750", status: "Approved")
+      ]
     
     
     override func viewDidLoad() {
@@ -65,7 +88,10 @@ class ViewController: UIViewController {
         sender.backgroundColor = .white
         monthlyBtn.tintColor = .black
         
-        let monthlyTtl =  expensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.reduce(0.0){$0 + ($1 ?? 0)}
+        expensiveDetails = MonthlyExpensiveDetails
+        expensiveCollectionView.reloadData()
+        
+        let monthlyTtl =  MonthlyExpensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.reduce(0.0){$0 + ($1 ?? 0)}
         
         ttlAmntLbl.text = "₹" + String(monthlyTtl)
     }
@@ -77,9 +103,12 @@ class ViewController: UIViewController {
         sender.backgroundColor = .white
         weeklyBtn.tintColor = .black
         
-        let weeklyTtl =  expensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.reduce(0.0){$0 + ($1 ?? 0)}
+        expensiveDetails = weeklyExpensiveDetails
+        expensiveCollectionView.reloadData()
         
-        ttlAmntLbl.text = "₹" + String(weeklyTtl/7)
+        let weeklyTtl =  weeklyExpensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.compactMap{$0}.reduce(0.0){$0 + $1}
+
+        ttlAmntLbl.text = "₹" + String(weeklyTtl)
     }
     
     @IBAction func dailyBtnTapped(_ sender: UIButton) {
@@ -89,9 +118,12 @@ class ViewController: UIViewController {
         sender.backgroundColor = .white
         dailyBtn.tintColor = .black
         
-        let weeklyTtl =  expensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.reduce(0.0){$0 + ($1 ?? 0)}
+        expensiveDetails = DailyExpensiveDetails
+        expensiveCollectionView.reloadData()
         
-        ttlAmntLbl.text = "₹" + String(weeklyTtl/30)
+        let weeklyTtl =  DailyExpensiveDetails.map(\.itemPrice).compactMap{$0}.map{Double($0)}.reduce(0.0){$0 + ($1 ?? 0)}
+        
+        ttlAmntLbl.text = "₹" + String(weeklyTtl)
         
     }
   
@@ -105,6 +137,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
             
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
        return expensiveDetails.count
     }
     
